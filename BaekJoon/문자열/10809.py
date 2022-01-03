f84f1bbe1118efa5b28730675503801d0cffe0df
seungbeom1010 <seungbeom.dev@gmail.com>
2022-01-04 00:18:04 +0900

    문자열 풀이

---
+++ BaekJoon/문자열/10809.py
@@ -0,0 +1,10 @@
+
+S = input()
+
+a_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in a_list:
+    if i in S:
+        print(S.index(i), end = " ")
+    else:
+        print(-1, end = ' ')
